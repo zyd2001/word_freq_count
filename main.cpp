64a1455d20a47b091803c8eef34e46bdc91b2c18
zyd2001 <zyd200101@outlook.com>
2017-01-21 10:23:44 +0800

    read lem

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+struct slist
+{
+	string head;
+	vector<string> var;
+};
+
 struct storage
 {
 	string name;
@@ -13,11 +19,13 @@ struct storage
 };
 
 vector<storage> vi;
+vector<slist> vlem;
 
 void fprocess(ifstream&);
 //void outfile(ofstream&);
 //string lower(string);
 string process(string);
+void readLem(ifstream&);
 
 class findstr
 {
@@ -28,12 +36,18 @@ private:
 	string str;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
-	ifstream fin("input.txt"), fsave;
+	ifstream fin, fsave, flemm("2+2+3lem.txt");
 	ofstream fout;
-	string temp;
+	string temp, filename;
+	if (argc == 2)
+		filename = argv[1];
+	else
+		filename = "input.txt";
 	vector<storage>::iterator result;
+	readLem(flemm);
+	fin.open(filename.c_str());
 	fsave.open("save.txt");
 	if (fsave.is_open())
 	{
@@ -80,12 +94,57 @@ string process(string str)
 {
 	for (int i = 0; i < str.size(); i++)
 	{
-		if (!isalpha(str[i])) 
+		if (str.substr(i, 2) == "\'s" || str.substr(i, 2) == "\'d" || str.substr(i, 2) == "\'m")
+		{
+			str.erase(i, 2);
+			i--;
+			continue;
+		}
+		if (str.substr(i, 3) == "n\'t" || str.substr(i, 3) == "\'ve" || str.substr(i, 3) == "\'ll")
+		{
+			str.erase(i, 3);
+			i--;
+			continue;
+		}
+		if (!isalpha(str[i]) && str[i] != '-')
 		{
 			str.erase(i, 1);
 			i--;
+			continue;
 		}
 		str[i] = tolower(str[i]);
 	}
 	return str;
+}
+
+void readLem(ifstream &fin)
+{
+	string strtemp;
+	fin >> strtemp;
+	while (fin.peek() != EOF)
+	{
+		slist temp;
+		temp.head = strtemp;
+		fin >> strtemp;
+		if (strtemp[0] == '#')
+		{
+			string sub;
+			sub = strtemp.substr(1, strtemp.size());
+			int index = sub.find('#', 0);
+			while (index != sub.npos)
+			{
+				temp.var.push_back(sub.substr(0, index));
+				sub = sub.substr(index + 1, sub.size());
+				index = sub.find('#', 0);
+			}
+			temp.var.push_back(sub);
+			vlem.push_back(temp);
+			fin >> strtemp;
+		}
+		else
+		{
+			vlem.push_back(temp);
+			continue;
+		}
+	}
 }
